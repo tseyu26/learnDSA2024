eaf5f935fd9452db31e201e9b4dc0ab5f7a42142
tseyu <newbeeu2022@outlook.com>
2024-08-16 11:00:53 +0800

    add Arr

---
+++ ArrayList.java
@@ -0,0 +1,16 @@
+import java.util.Random;;
+public class ArrayList{
+    public static void main(String[] args){
+        int[] array = new int[10];
+        Random random = new Random();
+        for (int element=0;element < array.length;element++){
+            array[element] = random.nextInt();
+        }
+        for (int element:array){
+            System.out.println(element + " ");
+        }
+    }
+}
+// It is a duplication while using for-each
+// assigning the ele can only use traditional for
+// read can use for-each 
