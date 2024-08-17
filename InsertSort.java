678aacd01c34f1b6907458c0ea1dd40f6de4ce54
tseyu <newbeeu2022@outlook.com>
2024-08-17 11:06:49 +0800

    I

--- InsertSort.java
+++ InsertSort.java
@@ -1,5 +1,12 @@
 public class InsertSort {
     public static int[] insertSort(int[] array){
+        for (int i = 1; i < array.length;i++){
+            int j = i;
+            while (j > 0 && array[j] < array[j - 1]){
+                SwapArrayEle.SwapEle(array, j-1, j);
+                j--;
+            }  
+        }
         return array;
     }
     public static void main(String[] args) {
