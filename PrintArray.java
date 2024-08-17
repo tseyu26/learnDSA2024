4ec5045a75404a3813706338a800560a4ccd0a80
tseyu <newbeeu2022@outlook.com>
2024-08-17 15:49:40 +0800

    m

--- PrintArray.java
+++ PrintArray.java
@@ -1,8 +1,9 @@
 public class PrintArray {
     public static void printArray(int[] array){
         for (int ele: array){
-            System.out.println(ele);
+            System.out.print (ele + " ");
         }
+        System.out.println();
     }
     public static void main(String[] args){
         int[] intArray = new int[10];
